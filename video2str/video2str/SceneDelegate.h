c6bddebe5c226b8071b4004129198459f8ec9f3a
邱开禄 <944624367@qq.com>
2019-12-31 13:14:24 +0800

    【feature】Init Project

---
+++ video2str/video2str/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  video2str
+//
+//  Created by 邱开禄 on 2019/12/31.
+//  Copyright © 2019 邱开禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ video2str/video2str/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video2str
+//
+//  Created by 邱开禄 on 2019/12/31.
+//  Copyright © 2019 邱开禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
